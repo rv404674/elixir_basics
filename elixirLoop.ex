c585daee20a01d781f5fe582bb37f32265cc9443
Rahul Verma <rv404674@gmail.com>
2021-07-03 03:08:25 +0530

    List comprehensions and enum

--- elixirLoop.ex
+++ elixirLoop.ex
@@ -11,11 +11,33 @@ defmodule M do
     # NOTE: Recursive fn to do looping
     if min > max do
       loop(0, max)
-    else dd
+    else
       IO.puts("NUM: #{min}")
       loop(min + 1, max)
     end
   end
+
+  def enum_basics do
+    IO.puts "Even List: #{
+      Enum.all?(
+        [1, 2, 3, 4],
+        fn (n) -> rem(n, 2) === 0 end
+      )
+    }"
+
+    Enum.each([1,2,3], fn(n) -> IO.puts(n) end)
+    db_list_map = Enum.map([1,2,3], fn(n) -> n*2 end)
+    IO.inspect db_list_map
+  end
+
+  def list_comprehension do
+    db_list = for n <- [1,2,3], do: n * 2
+    IO.inspect db_list
+
+    even_list = for n <- [1,2,3,4], rem(n,2) == 0, do: n
+    IO.inspect even_list
+  end
+
 
 
 end
